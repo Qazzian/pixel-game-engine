6448191a38b8781fa477947b4874b7040ee5d5e0
Qazzian <Qazzian@gmail.com>
2022-06-29 00:24:12 +0100

    Export colour constants

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export {PixelGameEngine as PixelGameEngine} from './src/PixelGameEngine';
-export {Colour} from "./src/Colour";
-export {Colour as Color} from "./src/Colour";
+export {Colour, COLOURS} from "./src/Colour";
+export {Colour as Color, COLORS} from "./src/Colour";
 export {Grid} from "./src/Grid";
