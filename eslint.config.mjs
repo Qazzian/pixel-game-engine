88ec47fa33d2afd43f2ef37737b76de370ea77f2
Qazzian <Qazzian@gmail.com>
2025-07-26 22:13:00 +0100

    Add eslint-plugin-jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import pluginJest from "eslint-plugin-jest";
 
 export default tseslint.config(
 	{
@@ -15,4 +16,19 @@ export default tseslint.config(
 			tseslint.configs.recommended,
 		]
 	},
+	{
+		files: ['**/*.test.js', '**/*.test.jsx'],
+		ignores: ["dist/**", "esm/**"],
+		plugins: { pluginJest },
+		languageOptions: {
+			globals: pluginJest.environments.globals.globals,
+		},
+		rules: {
+			'jest/no-disabled-tests': 'warn',
+			'jest/no-focused-tests': 'error',
+			'jest/no-identical-title': 'error',
+			'jest/prefer-to-have-length': 'warn',
+			'jest/valid-expect': 'error',
+		},
+	}
 );
