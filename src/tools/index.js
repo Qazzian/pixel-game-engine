35e707a0f43a6597f7acdf8718f89c8aa010e578
Qazzian <Qazzian@gmail.com>
2025-07-26 12:14:47 +0100

    Remove compiled files

--- src/tools/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Random = exports.collision = void 0;
-var collision_1 = require("./collision");
-Object.defineProperty(exports, "collision", { enumerable: true, get: function () { return collision_1.default; } });
-var Random_1 = require("./Random");
-Object.defineProperty(exports, "Random", { enumerable: true, get: function () { return Random_1.Random; } });
