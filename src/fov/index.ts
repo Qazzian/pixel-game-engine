1127debc8a874f5803097ce196fc3d821e00465f
Qazzian <Qazzian@gmail.com>
2025-11-30 21:08:15 +0000

    Replace Position obj with Point interface
    Gives functions that don't need the Position functions more flexibility.
    Also removed a duplicate Point interface in fov

--- src/fov/index.ts
+++ src/fov/index.ts
@@ -2,5 +2,4 @@ export { buildFov } from './buildFov.js';
 export { type Intersect } from './buildFov.js';
 export { buildGeometry } from './buildGeometry.js';
 export { Edge } from './Edge.js';
-export { type Point } from './point.js';
 export { Ray } from './Ray.js';
